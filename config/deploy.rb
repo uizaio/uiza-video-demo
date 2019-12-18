8d0660d30edfc28091209510c51160cede62856e
hoptq91 <dev.hoptq@gmail.com>
2019-12-18 16:25:20 +0700

    Finish install server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-Change these
+#Change these
 server '35.198.213.120', port: 22, roles: [:web, :app, :db], primary: true
 set :repo_url,        'git@github.com:uizaio/uiza-video-demo.git'
 set :application,     'uiza'

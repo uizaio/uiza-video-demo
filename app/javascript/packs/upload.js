71e70983a064888e6f00e12b41e521ff75f992fe
quanbh <quanbh1911@gmail.com>
2019-12-19 12:25:42 +0700

    add theme progress and success upload video

--- app/javascript/packs/upload.js
+++ app/javascript/packs/upload.js
@@ -1,12 +1,19 @@
 $(document ).ready(function() {
-  dropZone.ondragover = dropZone.ondragenter = function(evt) {
-    evt.preventDefault();
-  };
-  
-  dropZone.ondrop = function(evt) {
-    fileInput.files = evt.dataTransfer.files;
-    evt.preventDefault();
-    console.log(fileInput.files);
-  };
-
+  if($('#dropZone').length) {
+    dropZone.ondragover = dropZone.ondragenter = function(evt) {
+      evt.preventDefault();
+    };
+    dropZone.ondrop = function(evt) {
+      fileInput.files = evt.dataTransfer.files;
+      evt.preventDefault();
+      console.log(fileInput.files);
+    };
+  }
+  $('.close-btn').click(function(){
+    let className = $(this).closest('.popup-sign-up').attr('class');
+    if(className){
+      let JElement = "." + className;
+      $(JElement).css("display", "none");
+    }
+  })
 });

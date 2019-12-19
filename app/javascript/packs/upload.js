9345829d5967a560df728e4f2da82635aa6f91cc
quanbh <quanbh1911@gmail.com>
2019-12-19 16:52:53 +0700

    add modal upload video fail

--- app/javascript/packs/upload.js
+++ app/javascript/packs/upload.js
@@ -16,4 +16,16 @@ $(document ).ready(function() {
       $(JElement).css("display", "none");
     }
   })
+  $("#myBtn").click(function(){
+    $("#upload-fail-modal").modal();
+  });
+  $("#upload-fail-modal").modal();
+  function hidenModal(selectorElement, targetModal){
+    $(selectorElement).click(function(){
+      $(targetModal).modal('hide');
+    })
+    
+  }
+  hidenModal('.btn-close-modal', '#upload-fail-modal');
+  hidenModal('.close-icon-modal', '#upload-fail-modal');
 });

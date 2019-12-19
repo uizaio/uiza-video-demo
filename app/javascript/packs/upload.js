63cf55cc55b33ee6ee384a17e68775e7ac0760a5
quanbh <quanbh1911@gmail.com>
2019-12-20 01:55:21 +0700

    add logic upload file

--- app/javascript/packs/upload.js
+++ app/javascript/packs/upload.js
@@ -6,7 +6,8 @@ $(document ).ready(function() {
     dropZone.ondrop = function(evt) {
       fileInput.files = evt.dataTransfer.files;
       evt.preventDefault();
-      console.log(fileInput.files);
+      upload_files_with_progress(fileInput.files[0]);
+
     };
   }
   $('.close-btn').click(function(){
@@ -16,10 +17,6 @@ $(document ).ready(function() {
       $(JElement).css("display", "none");
     }
   })
-  $("#myBtn").click(function(){
-    $("#upload-fail-modal").modal();
-  });
-  $("#upload-fail-modal").modal();
   function hidenModal(selectorElement, targetModal){
     $(selectorElement).click(function(){
       $(targetModal).modal('hide');
@@ -28,4 +25,71 @@ $(document ).ready(function() {
   }
   hidenModal('.btn-close-modal', '#upload-fail-modal');
   hidenModal('.close-icon-modal', '#upload-fail-modal');
+
+  $('#fileInput').on("change", function(){ 
+    let file = $(this).prop('files');
+    upload_files_with_progress(file[0]);
+   });
+
+  function upload_files_with_progress(fileData) {
+    var formdata = new FormData();
+    formdata.append("name", fileData.name);
+    formdata.append("data", fileData);
+    var ajax = new XMLHttpRequest();
+    ajax.upload.addEventListener("progress", progressHandler);
+    ajax.addEventListener("error", errorHandler);
+    ajax.addEventListener("abort", abortHandler);
+    ajax.open("POST", "api/v1/upload");
+    ajax.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if(this.status == 200) {
+          videoUploadSuccess(this.response);
+        }else {
+          videoUploadFail(this.response);
+        }
+
+      }     
+    }; 
+    ajax.send(formdata); 
+
+  }
+  function progressHandler(e) {
+    $('#upload-video-block').css("display", "none");
+    $('#edit-upload').css("display", "block");
+    $('.upload-video-success .upload-video-fail').css("display", "none");
+    $('.upload-video-progress').css("display", "block");
+    let progressBarFill = document.querySelector("#progress-upload > .progress-bar-load");
+    let progressBarText = document.querySelector("#value-progres");
+    let percent  = e.lengthComputable ?  (e.loaded / e.total) * 100 : 0;
+    progressBarFill.style.width = percent.toFixed(0) + "%";
+    progressBarText.textContent = percent.toFixed(0) + "%";
+  }
+  function errorHandler(e) {
+    console.log(e);
+  }
+  
+  function abortHandler(e) {
+    console.log(e)
+  }
+  function videoUploadSuccess (res) {
+    let resObj = JSON.parse(res);
+    $('#upload-video-block').css("display", "none");
+    $('#edit-upload').css("display", "block");
+    $('.upload-video-progress').css("display", "none")
+    $('.upload-video-fail').css("display", "none");
+    $('.upload-video-success').css("display", "block");
+    let videoName = resObj.data.upload.name;
+    console.log(videoName);
+    let videoNameText = document.querySelector("#video-name");
+    videoNameText.textContent = videoName;
+  }
+  function videoUploadFail(res) {
+    $('#upload-video-block').css("display", "none");
+    $('#edit-upload').css("display", "block");
+    $('.upload-video-fail').css("display", "block");
+    $('.upload-video-progress').css("display", "none")
+    $('.upload-video-success').css("display", "none");
+    $("#upload-fail-modal").modal();
+  }
+  
 });

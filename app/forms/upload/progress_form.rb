cade968396a85565a72ea1ca1b67441d3b1f50cb
hoptq91 <dev.hoptq@gmail.com>
2019-12-20 08:46:54 +0700

    Uncomment form

--- app/forms/upload/progress_form.rb
+++ app/forms/upload/progress_form.rb
@@ -1,20 +1,20 @@
-# class Upload::ProgressForm
-#   include ActiveModel::Model
-#   attr_accessor :upload_data, :upload_code
+class Upload::ProgressForm
+  include ActiveModel::Model
+  attr_accessor :upload_data, :upload_code
 
-#   validates :upload_data, presence: true
+  validates :upload_data, presence: true
 
-#   def initialize(params)
-#     @upload_data = params[:data]
-#     @upload_code = params[:code]
-#   end
+  def initialize(params)
+    @upload_data = params[:data]
+    @upload_code = params[:code]
+  end
 
-#   def progress
-#     if valid?
-#       upload_service = UploadService.new()
-#       upload_service.progress(upload_code, upload_data)
-#     else
-#       [false, errors.messages, 400]
-#     end
-#   end
-# end
+  def progress
+    if valid?
+      upload_service = UploadService.new()
+      upload_service.progress(upload_code, upload_data)
+    else
+      [false, errors.messages, 400]
+    end
+  end
+end

643495e3cd6784f976ba84352dd162827f5a0777
hoptq91 <dev.hoptq@gmail.com>
2019-12-20 08:37:28 +0700

    Fix raven get DNS

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   Raven.configure do |config|
-    config.dsn = env['RAVEN_DNS']
+    config.dsn = ENV['RAVEN_DNS']
     config.environments = %w[ production ]
   end
 end

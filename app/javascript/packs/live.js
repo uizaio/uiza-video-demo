3d1a12b33c6ace08bcc8edf5f7537b7d0104926a
quanbh <quanbh1911@gmail.com>
2019-12-24 10:30:07 +0700

    intergrated live index

--- app/javascript/packs/live.js
+++ app/javascript/packs/live.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
+
   $('#create-live-btn').click(function(){
     let liveName = $(".live-name").val();
     let liveDes = $(".live-des").val();
+    if(liveName){
+      formData = new FormData();
+      formData.append("name", liveName);
+      formData.append("des", liveDes);
+      $.ajax({
+        url: '/api/v1/lives',
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        method: 'POST',
+        success: function(response)
+        {
+          // console.log(response.data.live.code);
+          redirect_to_streaming_view(response.data.live.code);
+        },
+        error: function (error)
+        {
+          console.log(error);
+        }
+      });
+    }else {
+      alert('Không được để trống name');
+    }
   });
+  function redirect_to_streaming_view(code){
+    let currentURL = window.location.href;
+    let urlStreaming = currentURL + "/" + code;
+    window.location.href = urlStreaming;
+  }
+
 })

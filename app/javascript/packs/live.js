2c62b544de283489562e072da8e0c5e652ab3ec7
quanbh <quanbh1911@gmail.com>
2019-12-24 15:16:52 +0700

    add popup

--- app/javascript/packs/live.js
+++ app/javascript/packs/live.js
@@ -14,33 +14,38 @@ $(document).ready(function() {
       formData = new FormData();
       formData.append("name", liveName);
       formData.append("des", liveDes);
-      $.ajax({
-        url: '/api/v1/lives',
-        data: formData,
-        cache: false,
-        contentType: false,
-        processData: false,
-        method: 'POST',
-        success: function(response)
-        {
-          // console.log(response.data.live.code);
-          redirect_to_streaming_view(response.data.live.code);
-        },
-        error: function (error)
-        {
-          console.log(error);
-        }
-      });
+      $('#process-modal-live').modal();
+      // $('#process-modal-live').modal({backdrop: 'static', keyboard: false});
+      // $.ajax({
+      //   url: '/api/v1/lives',
+      //   data: formData,
+      //   cache: false,
+      //   contentType: false,
+      //   processData: false,
+      //   method: 'POST',
+      //   success: function(response)
+      //   {
+      //     // console.log(response.data.live.code);
+      //     redirect_to_streaming_view(response.data.live.code);
+      //   },
+      //   error: function (error)
+      //   {
+      //     console.log(error);
+      //   }
+      // });
     }else {
       alert('Name and Description not empty');
     }
   });
+  $('.close-icon-modal-live').click(function(){
+    $('#process-modal-live').modal('hide');
+  })
   function redirect_to_streaming_view(code){
     let location = window.location;
 
     let currentURL = location.origin;
-    let urlStreaming = currentURL + "/lives/" + code;
-    window.location.href = urlStreaming;
+    let urlStreaming = currentURL + "/lives/" + code + "/detail";
+    // window.location.href = urlStreaming;
   }
   $('#copy-stream-url').click(function(){
     copyToClipboard($('.stream-url-input').val());

e8f310a2317a85dc816f4dec96fef80510e04fe1
hoptq91 <dev.hoptq@gmail.com>
2019-12-26 10:14:04 +0700

    Update live service logic

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -5,17 +5,25 @@ class LivesController < ApplicationController
 
   def detail
     @live = Live.find_by_code(params[:code]).first
-    uiza_service = UizaService.new(ENV['UIZA_LIVE_BASE_API'])
-    uiza_live_info = uiza_service.live_entity(@live.uiza_id)
-    if uiza_live_info && uiza_live_info['ingest']
+    uiza_service = UizaService.new(ENV['UIZA_LIVE_API_KEY'])
+    @uiza_live_info = uiza_service.live_entity(@live.uiza_id)
+    if @uiza_live_info && @uiza_live_info['ingest']
       @live.update(
-        stream_url: uiza_live_info['ingest']['url'],
-        stream_key: uiza_live_info['ingest']['key']
+        stream_url: @uiza_live_info['ingest']['url'],
+        stream_key: @uiza_live_info['ingest']['key']
       )
     end
   end
 
   def show
     @live = Live.find_by_code(params[:code]).first
+    uiza_service = UizaService.new(ENV['UIZA_LIVE_API_KEY'])
+    @uiza_live_info = uiza_service.live_entity(@live.uiza_id)
+    if @uiza_live_info && @uiza_live_info['ingest']
+      @live.update(
+        stream_url: @uiza_live_info['ingest']['url'],
+        stream_key: @uiza_live_info['ingest']['key']
+      )
+    end
   end
 end
